455d108fa66923a2c95c62b79b3103edc23a3f7f
LaykinK <77702332+LaykinK@users.noreply.github.com>
2023-04-27 14:40:57 -0500

    Add files via upload

---
+++ UpdatedFiles/allergens.js
@@ -0,0 +1,126 @@
+
+var milk = true;
+function setPreferenceMilk()
+{
+   if (document.querySelector(".milk").style.background = "#5DA271") {
+        document.querySelector(".milk").style.background = "#EE6C4D";
+        milk = false;
+   }
+   else if(document.querySelector(".milk").style.background = "#EE6C4D") {
+         document.querySelector(".milk").style.background = "#5DA271";
+        milk = true;
+    }
+   
+} 
+
+var eggs = true;
+function setPreferenceEggs()
+{
+   if (document.querySelector(".eggs").style.background = "#5DA271") {
+        document.querySelector(".eggs").style.background = "#EE6C4D";
+        eggs = false;
+   }
+   else if(document.querySelector(".eggs").style.background = "#EE6C4D") {
+         document.querySelector(".eggs").style.background = "#5DA271";
+         eggs = true;
+    }
+   
+} 
+
+var peanuts = true;
+function setPreferencePeanuts()
+{
+   if (document.querySelector(".peanuts").style.background = "#5DA271") {
+        document.querySelector(".peanuts").style.background = "#EE6C4D";
+        peanuts = false;
+   }
+   else if(document.querySelector(".peanuts").style.background = "#EE6C4D") {
+         document.querySelector(".peanuts").style.background = "#5DA271";
+         peanuts = true;
+    }
+   
+} 
+
+var treenuts = true;
+function setPreferenceTreeNuts()
+{
+   if (document.querySelector(".treenuts").style.background = "#5DA271") {
+        document.querySelector(".treenuts").style.background = "#EE6C4D";
+        treenuts = false;
+   }
+   else if(document.querySelector(".treenuts").style.background = "#EE6C4D") {
+         document.querySelector(".treenuts").style.background = "#5DA271";
+         treenuts = true;
+    }
+   
+} 
+
+var fish = true;
+function setPreferenceFish()
+{
+   if (document.querySelector(".fish").style.background = "#5DA271") {
+        document.querySelector(".fish").style.background = "#EE6C4D";
+        fish = false;
+   }
+   else if(document.querySelector(".fish").style.background = "#EE6C4D") {
+         document.querySelector(".fish").style.background = "#5DA271";
+         fish = true;
+    }
+   
+} 
+
+var crustaceans = true;
+function setPreferenceCrustaceans()
+{
+   if (document.querySelector(".crustaceans").style.background = "#5DA271") {
+        document.querySelector(".crustaceans").style.background = "#EE6C4D";
+        crustaceans = false;
+   }
+   else if(document.querySelector(".crustaceans").style.background = "#EE6C4D") {
+         document.querySelector(".crustaceans").style.background = "#5DA271";
+         crustaceans = true;
+    }
+   
+} 
+
+var wheat = true;
+function setPreferenceWheat()
+{
+   if (document.querySelector(".wheat").style.background = "#5DA271") {
+        document.querySelector(".wheat").style.background = "#EE6C4D";
+        wheat = false;
+   }
+   else if(document.querySelector(".wheat").style.background = "#EE6C4D") {
+         document.querySelector(".wheat").style.background = "#5DA271";
+         wheat = true;
+    }
+   
+} 
+
+var soy = true;
+function setPreferenceSoy()
+{
+   if (document.querySelector(".soy").style.background = "#5DA271") {
+        document.querySelector(".soy").style.background = "#EE6C4D";
+        soy = false;
+   }
+   else if(document.querySelector(".soy").style.background = "#EE6C4D") {
+         document.querySelector(".soy").style.background = "#5DA271";
+         soy = true;
+    }
+   
+} 
+
+var sesame = true;
+function setPreferenceSesame()
+{
+   if (document.querySelector(".sesame").style.background = "#5DA271") {
+        document.querySelector(".sesame").style.background = "#EE6C4D";
+        sesame = false;
+   }
+   else if(document.querySelector(".sesame").style.background = "#EE6C4D") {
+         document.querySelector(".sesame").style.background = "#5DA271";
+         sesame = true;
+    }
+   
+} 
