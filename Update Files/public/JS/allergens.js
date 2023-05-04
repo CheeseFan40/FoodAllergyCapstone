1220405e0363805dbe83f8166e4e9f1ed911afa9
LaykinK <77702332+LaykinK@users.noreply.github.com>
2023-05-03 23:50:03 -0500

    Add files via upload

---
+++ Update Files/public/JS/allergens.js
@@ -0,0 +1,172 @@
+console.log("Connected");
+
+var milkCount = 1;
+var milk = true;
+function setPrefMilk()
+{
+     const milkColor = document.querySelector(".milk");
+     if (milkCount == 1){
+          milkColor.style.background = "#EE6C4D";
+          milk = false;
+          milkCount = 2;
+          console.log("Set orange");
+     }
+     else if (milkCount == 2){
+          milkColor.style.background = "#5DA271";
+          milk = true;
+          milkCount = 1;
+          console.log("Set green");
+     }   
+}
+
+var eggsCount = 1;
+var eggs = true;
+function setPrefEggs()
+{
+     const eggsColor = document.querySelector(".eggs");
+     if (eggsCount == 1){
+          eggsColor.style.background = "#EE6C4D";
+          eggs = false;
+          eggsCount = 2;
+          console.log("Set orange");
+     }
+     else if (eggsCount == 2){
+          eggsColor.style.background = "#5DA271";
+          eggs = true;
+          eggsCount = 1;
+          console.log("Set green");
+     }   
+} 
+
+var peanutsCount = 1;
+var peanuts = true;
+function setPrefPeanuts()
+{
+     const peanutsColor = document.querySelector(".peanuts");
+     if (peanutsCount == 1){
+          peanutsColor.style.background = "#EE6C4D";
+          peanuts = false;
+          peanutsCount = 2;
+          console.log("Set orange");
+     }
+     else if (peanutsCount == 2){
+          peanutsColor.style.background = "#5DA271";
+          peanuts = true;
+          peanutsCount = 1;
+          console.log("Set green");
+     }   
+}
+
+var treenutsCount = 1;
+var treenuts = true;
+function setPrefTreeNuts()
+{
+     const treenutsColor = document.querySelector(".treenuts");
+     if (treenutsCount == 1){
+          treenutsColor.style.background = "#EE6C4D";
+          treenuts = false;
+          treenutsCount = 2;
+          console.log("Set orange");
+     }
+     else if (treenutsCount == 2){
+          treenutsColor.style.background = "#5DA271";
+          treenuts = true;
+          treenutsCount = 1;
+          console.log("Set green");
+     }   
+}
+
+var fishCount = 1;
+var fish = true;
+function setPrefFish()
+{
+     const fishColor = document.querySelector(".fish");
+     if (fishCount == 1){
+          fishColor.style.background = "#EE6C4D";
+          fish = false;
+          fishCount = 2;
+          console.log("Set orange");
+     }
+     else if (fishCount == 2){
+          fishColor.style.background = "#5DA271";
+          fish = true;
+          fishCount = 1;
+          console.log("Set green");
+     }   
+}
+
+var crustaceansCount = 1;
+var crustaceans = true;
+function setPrefCrustaceans()
+{
+     const crustaceansColor = document.querySelector(".crustaceans");
+     if (crustaceansCount == 1){
+          crustaceansColor.style.background = "#EE6C4D";
+          crustaceans = false;
+          crustaceansCount = 2;
+          console.log("Set orange");
+     }
+     else if (crustaceansCount == 2){
+          crustaceansColor.style.background = "#5DA271";
+          crustaceans = true;
+          crustaceansCount = 1;
+          console.log("Set green");
+     }   
+}
+
+var wheatCount = 1;
+var wheat = true;
+function setPrefWheat()
+{
+     const wheatColor = document.querySelector(".wheat");
+     if (wheatCount == 1){
+          wheatColor.style.background = "#EE6C4D";
+          wheat = false;
+          wheatCount = 2;
+          console.log("Set orange");
+     }
+     else if (wheatCount == 2){
+          wheatColor.style.background = "#5DA271";
+          wheat = true;
+          wheatCount = 1;
+          console.log("Set green");
+     }   
+}
+
+var soyCount = 1;
+var soy = true;
+function setPrefSoy()
+{
+     const soyColor = document.querySelector(".soy");
+     if (soyCount == 1){
+          soyColor.style.background = "#EE6C4D";
+          soy = false;
+          soyCount = 2;
+          console.log("Set orange");
+     }
+     else if (soyCount == 2){
+          soyColor.style.background = "#5DA271";
+          soy = true;
+          soyCount = 1;
+          console.log("Set green");
+     }   
+}
+
+var sesameCount = 1;
+var sesame = true;
+function setPrefSesame()
+{
+     const sesameColor = document.querySelector(".sesame");
+     if (sesameCount == 1){
+          sesameColor.style.background = "#EE6C4D";
+          sesame = false;
+          sesameCount = 2;
+          console.log("Set orange");
+     }
+     else if (sesameCount == 2){
+          sesameColor.style.background = "#5DA271";
+          sesame = true;
+          sesameCount = 1;
+          console.log("Set green");
+     }   
+}
---
+++ Update Files/server.js
@@ -0,0 +1,302 @@
+if (process.env.NODE_ENV !=='production'){
+    require('dotenv').config()
+}
+
+// Required Modules
+const mongoose = require('mongoose');
+const express = require('express')
+
+const app = express()
+const port = 3000
+const Schema = mongoose.Schema;
+
+
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+
+// Sets up the Database Connection
+mongoose.connect("mongodb+srv://MitchZ:T2GRPvC0AkjRuDL8@cluster0.edbmsue.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then((result) => {
+    console.log('MongoDB Connection Established!')
+
+}).catch((err) => {
+    console.log(err)
+});
+  
+// Defining User schema
+const userSchema = new Schema(
+    {RestaurantName: String, Milk: Array, Peanuts: Array, Soy: Array, Wheat: Array, Eggs: Array, Treetnut: Array, Shellfish: Array, Sesame: Array, Fish: Array }
+)
+
+  
+// Defining User model
+const User = mongoose.model('User', userSchema);
+  
+
+// Create collection of Model
+User.createCollection().then(function (collection) {
+    console.log('Collection is created!');
+});
+
+
+const userSchema2 = new Schema(
+  {ID: String, UserName: String, Email: String, HashedPassword: String, Milk: String, Peanuts: String, Soy: String, Wheat: String, Eggs: String, Treenut: String, Shellfish: String, Sesame: String, Fish: String}
+)
+const Accounts = mongoose.model('accounts',userSchema2)
+
+Accounts.createCollection().then(function (collection){
+  console.log('Collection2 is created ')
+});
+
+
+
+//Sets empty array
+const usrs = []
+
+//Creates and authenticates password
+const createPassport = require('./passportConfig');//exported as initialized
+
+createPassport(
+    passport,
+    email => usrs.find(user => user.email === email),
+    id => usrs.find(user => user.id === id)
+)//initialized function that is initialize(passport, getUserByEmail, getUserById)
+
+
+//looks for the ejs file in the view folder
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+//to send warnings that the password is incorrect
+app.use(flash())
+//used to save info on the server
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+//Calls from passportConfig.js
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+
+// Checks the name with what is visible
+app.get('/', checkAuthenticated, (req, res) => { 
+    res.render('index.ejs', {name: req.user.name})
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.ejs')
+})
+app.post('/about')
+
+app.get('/account-log-in', checkNotAuthenticated, (req, res) => {
+    res.render('account-log-in.ejs')
+})
+
+
+// NOT WORKING ???
+app.get('/account-logged-in', (req, res) => {
+    res.render('account-logged-in.ejs')
+})
+
+
+//Uses post to login and send to account-logged-in.ejs
+app.post('/account-log-in',checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/account-logged-in',
+    failureRedirect: '/account-log-in',
+    failureFlash: true
+}))
+
+//Gets and posts register page, requires email
+app.get('/account-sign-up', checkNotAuthenticated , (req, res) => {
+    res.render('account-sign-up.ejs')
+})
+
+//Gets user to sign up properly and redirects back if any field is missing
+app.post('/account-sign-up', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 8)
+        usrs.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        
+        console.log(usrs)
+        
+        const maper = usrs.map(x => x.email)
+        const maper2 = usrs.map(x => x.id)
+        const maper3 = usrs.map(x => x.name)
+        const maper4 = usrs.map(x => x.password)
+        let exists=''
+        for (let i = 0; i< maper.length; i++){
+            console.log(maper[i])
+            exists = await Accounts.exists({Email: maper[i]})
+            if(!exists){
+                Accounts.insertMany({ID: maper2[i], Email: maper[i], HashedPassword: maper4[i], UserName: maper3[i] })
+             
+                //Accounts.insertMany({HashedPassword: password})
+            }else{
+                
+                console.log("already exists")
+    
+                
+            }
+        }
+
+        
+        
+
+        res.redirect('/account-log-in')
+    } catch {
+        res.redirect('/account-sign-up')
+    }
+    
+})
+
+//logs usr out
+app.delete('logout', (req, res) => {
+    req.logOut()
+    res.redirect('/account-log-in')
+})
+
+//function for verifying
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()){
+        return next()
+    }
+
+    res.redirect('/account-log-in')
+}
+
+//Checks auth and redirects to index.ejs
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+
+
+//Accesses the CSS and JS files in the public folder
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/CSS'))
+app.use('/js', express.static(__dirname + 'public/JS'))
+//allows images
+app.use('/png', express.static(__dirname + 'public/Images'))
+
+app.listen(port, () => console.log(`Running on port ${port}`))
+
+
+
+// ********** RESTAURANT INPUT **********
+
+
+
+
+
+//function to see if a current document exists, if it doesn't create 
+//a new one according to the restaurant name input
+
+app.get('/owner', (req, res) => res.render("owner.ejs"))
+app.post('/owner', async function (req, res) {
+    try{    
+        const restarray = {
+            test: req.body.RestaurantNamer,
+            test1:  req.body.Milk,
+            test2 : req.body.Soy,
+            test3 :req.body.Peanuts,
+            test4: req.body.Wheat,
+            test5 :req.body.Eggs,
+            test6 :req.body.Treenut,
+            test7 : req.body.Shellfis,
+            test8 : req.body.Sesame,
+            test9 : req.body.Fish
+        }
+          
+   
+        
+    let exists = await User.exists({RestaurantName: restarray.test})
+    console.log(exists)
+
+    if(!exists){
+        console.log("not exist")
+        User.insertMany({RestaurantName: restarray.test})
+    }else{
+        console.log("exists")
+    }
+
+    if(restarray.test1 != ""){
+
+        await User.updateMany({RestaurantName:restarray.test},{ $push: {Milk:restarray.test1}}).then(function(){
+             console.log(`successfully added ${restarray.test1}`)
+             console.log(restarray.test1)
+    })
+    }
+    if(restarray.test2 != ""){
+
+        await User.updateMany({RestaurantName:restarray.test},{ $push: {Soy:restarray.test2}}).then(function(){
+            console.log(`successfully added ${restarray.test2}`)
+    })
+    }
+    if(test3 != ""){
+
+        await User.updateMany({RestaurantName:restarray.test},{ $push: {Peanuts:restarray.test3}}).then(function(){
+            console.log(`successfully added ${restarray.test3}`)
+    })
+    }
+    if(test4 != ""){
+
+        await User.updateMany({RestaurantName:restarray.test},{ $push: {Wheat:restarray.test4}}).then(function(){
+            console.log(`successfully added ${restarray.test4}`)
+    })
+    }
+    if(test5 != ""){
+
+        await User.updateMany({RestaurantName:restarray.test},{ $push: {Eggs:restarray.test5}}).then(function(){
+            console.log(`successfully added ${restarray.test5}`)
+    })
+    }
+    if(test6 != ""){
+
+        await User.updateMany({RestaurantName:restarray.test},{ $push: {Treenut:restarray.test6}}).then(function(){
+            console.log(`successfully added ${restarray.test6}`)
+    })
+    }
+    if(test7 != ""){
+
+        await User.updateMany({RestaurantName:restarray.test},{ $push: {Shellfish:restarray.test7}}).then(function(){
+            console.log(`successfully added ${restarray.test7}`)
+    })
+    }
+    if(test8 != ""){
+
+        await User.updateMany({RestaurantName:restarray.test},{ $push: {Sesame:restarray.test8}}).then(function(){
+            console.log(`successfully added ${restarray.test8}`)
+    })
+    }
+    if(test9 != ""){
+
+        await User.updateMany({RestaurantName:restarray.test},{ $push: {Fish:restarray.test9}}).then(function(){
+            console.log(`successfully added ${restarray.test9}`)
+    })
+    }
+
+    res.redirect('/owner')
+    } catch{
+    res.redirect('/owner')
+    
+    }
+    
+    
+    
+});
